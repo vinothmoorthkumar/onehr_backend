c4af05b7fc2f2cfdaf516cfa6567f4e5ff35b920
vinothtrigyn <vinoth.moorthy@trigyn.com>
2020-05-22 17:11:39 +0530

    login api done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -97,16 +97,16 @@ exports.register = [
 /**
  * User login.
  *
- * @param {string}      email
+ * @param {string}      username
  * @param {string}      password
  *
  * @returns {Object}
  */
 exports.login = [
-	body("email").isLength({ min: 1 }).trim().withMessage("Email must be specified.")
-		.isEmail().withMessage("Email must be a valid email address."),
+	body("username").isLength({ min: 1 }).trim().withMessage("Username must be specified.")
+		.withMessage("Username must be a valid email address."),
 	body("password").isLength({ min: 1 }).trim().withMessage("Password must be specified."),
-	sanitizeBody("email").escape(),
+	sanitizeBody("username").escape(),
 	sanitizeBody("password").escape(),
 	(req, res) => {
 		try {
@@ -114,42 +114,38 @@ exports.login = [
 			if (!errors.isEmpty()) {
 				return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
 			} else {
-				UserModel.findOne({ email: req.body.email }).then(user => {
+				UserModel.findOne({ username: req.body.username }).then(user => {
 					if (user) {
 						//Compare given password with db's hash.
 						bcrypt.compare(req.body.password, user.password, function (err, same) {
 							if (same) {
-								//Check account confirmation.
-								if (user.isConfirmed) {
-									// Check User's account active or not.
-									if (user.status) {
-										let userData = {
-											_id: user._id,
-											firstName: user.firstName,
-											lastName: user.lastName,
-											email: user.email,
-										};
-										//Prepare JWT token for authentication
-										const jwtPayload = userData;
-										const jwtData = {
-											expiresIn: process.env.JWT_TIMEOUT_DURATION,
-										};
-										const secret = process.env.JWT_SECRET;
-										//Generated JWT token with Payload and secret.
-										userData.token = jwt.sign(jwtPayload, secret, jwtData);
-										return apiResponse.successResponseWithData(res, "Login Success.", userData);
-									} else {
-										return apiResponse.unauthorizedResponse(res, "Account is not active. Please contact admin.");
-									}
+								// Check User's account active or not.
+								if (user.status) {
+									let userData = {
+										_id: user._id,
+										username: user.username,
+										superadmin: user.superadmin || false,
+										email: user.email,
+									};
+									//Prepare JWT token for authentication
+									const jwtPayload = userData;
+									const jwtData = {
+										expiresIn: process.env.JWT_TIMEOUT_DURATION,
+									};
+									const secret = process.env.JWT_SECRET;
+									//Generated JWT token with Payload and secret.
+									userData.token = jwt.sign(jwtPayload, secret, jwtData);
+									return apiResponse.successResponseWithData(res, "Login Success.", userData);
 								} else {
-									return apiResponse.unauthorizedResponse(res, "Account is not confirmed. Please confirm your account.");
+									return apiResponse.unauthorizedResponse(res, "Account is not active. Please contact admin.");
 								}
+
 							} else {
-								return apiResponse.unauthorizedResponse(res, "Email or Password wrong.");
+								return apiResponse.unauthorizedResponse(res, "Username or Password wrong.");
 							}
 						});
 					} else {
-						return apiResponse.unauthorizedResponse(res, "Email or Password wrong.");
+						return apiResponse.unauthorizedResponse(res, "Username or Password wrong.");
 					}
 				});
 			}
