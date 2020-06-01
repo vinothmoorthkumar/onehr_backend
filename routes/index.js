87966d77ce4a6bbaa235f83acc064a663ef6b173
vinothtrigyn <vinoth.moorthy@trigyn.com>
2020-06-01 17:27:21 +0530

    site api's added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var router = express.Router();
+const apiResponse = require("../helpers/apiResponse");
+
+const Page = require("../models/PageModel");
+
+function PageData(data) {
+    this.name = data.name;
+    this.slug = data.slug;
+    this.html = data.html;
+}
 
 /* GET home page. */
 router.get("/", function(req, res) {
 	res.render("index", { title: "Express" });
+});
+
+router.get("/site/:slug", function(req, res) {
+	try {
+		Page.findOne({}).then((page) => {
+			if (page !== null) {
+				let pageData = new PageData(page);
+				return apiResponse.successResponseWithData(res, "Operation success", pageData);
+			} else {
+				return apiResponse.successResponseWithData(res, "Operation success", {});
+			}
+		});
+	} catch (err) {
+		//throw error in json response with status 500. 
+		return apiResponse.ErrorResponse(res, err);
+	}
 });
 
 module.exports = router;
